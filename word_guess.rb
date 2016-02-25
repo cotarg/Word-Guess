13d638aef71bb9d1064093ae41fd6254a7152d38
Ania Gonzalez <aniagr@gmail.com>
2016-02-25 14:13:13 -0800

    Baseline requirements
    Wrote guess method for baseline

--- word_guess.rb
+++ word_guess.rb
@@ -1,3 +1,18 @@
 class WordGuess
+  def initialize
+    @guess = ""
+  end
 
-end 
+  def guess
+    puts "What letter do you guess?"
+    @guess = gets.chomp
+    puts @guess
+  end
+
+
+end
+
+
+class GameBoard
+
+end
