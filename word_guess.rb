4747b17198022d42a2cf410416cfb4078e13f9bd
Rowan Cota <cota.rg@gmail.com>
2016-02-25 15:48:49 -0800

    finished first pass guess logic
    need to refactor for wrong count decrementation! also how to send guess to game board!

--- word_guess.rb
+++ word_guess.rb
@@ -5,6 +5,7 @@ class WordGuess
     @guess = ""
     @wrong_count = 5 # Maybe will change this difficulty level
     @word = ["dog", "cat", "person"].sample #make up a dictionary later, hash??
+    @used_letters = []
   end
 
   def guess
@@ -13,6 +14,7 @@ class WordGuess
 
   end
 
+  # don't mess with this until we refactor
   def get_input
 
     # figure out the alphabet array and how to use a range
@@ -29,20 +31,31 @@ class WordGuess
 
     # echoes guess to user
     puts "Your guess was #{ @guess }."
-
-    #
   end
 
   def check_letter
 
+    unless @used_letters.include?(@guess) == false
+        puts "You have already guessed this letter!"
+        puts "Please guess again!"
+        @guess = gets.chomp
+    else
+         # add letter to used letters array
+        @used_letters.push(@guess)
+    end
+
     if @word.include?(@guess)
-
-      #if yes, check indexes
-
+        for index in 0..@word.length - 1
+            if @word[index] == @guess
+                # send to game board array
     else
       #guess is wrong
-
+      puts "You have made a terrible guess!"
+      puts "How could you pick that letter?!"
+      puts "You have #{ @wrong_count - 1 } guesses left."
     end
+
+    
 
 
   end
